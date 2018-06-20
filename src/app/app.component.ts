b33834e63fb3a300bdd6b7f63958f78728e0a635
Andy <andykeavey@hotmail.com>
2018-06-20 20:38:51 +0100

    Added light and dark theme with custom compoment styling and test theme switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  changeColour(): void {
+    const body = document.querySelector('body');
+    const curr = body.classList.contains('core-light');
+    if (curr) {
+      body.classList.remove('core-light');
+      body.classList.add('core-dark');
+    } else {
+      body.classList.remove('core-dark');
+      body.classList.add('core-light');
+    }
+    console.log('button pressed');
+  }
 }
