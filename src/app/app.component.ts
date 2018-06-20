ad63ade970618e10474ae2593f1c4e7e9bf79c3b
Andy <andykeavey@hotmail.com>
2018-06-20 22:26:47 +0100

    Moved theme mixin to style.scss from navbar component and added redbrick colour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent {
     const body = document.querySelector('body');
     const curr = body.classList.contains('core-light');
     if (curr) {
-      body.classList.remove('core-light');
+      body.className = '';
       body.classList.add('core-dark');
     } else {
-      body.classList.remove('core-dark');
+      body.className = '';
       body.classList.add('core-light');
     }
     console.log('button pressed');
