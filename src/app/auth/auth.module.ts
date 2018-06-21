9d2accd9b25e781a64dba690e89ecb5e019130a0
Andy <andykeavey@hotmail.com>
2018-06-21 13:21:31 +0100

    Added material form components to login form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,11 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule, MatCardModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginComponent, RegisterComponent]
 })
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+  password = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+  onSubmit() {
+    // const user = new User(
+    //     form.value.email,
+    //     form.value.password,
+    //     null
+    // );
+    // this.userService.login(user)
+    //     .subscribe(
+    //         data => {
+    //             this.email = data.body.email;
+    //         },
+    //         error => console.error(error)
+    //     );
+    //     form.reset();
+    }
 }
