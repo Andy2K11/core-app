d47ec7c5740c703137fa4421a468fc7d622b3edd
Andy <andykeavey@hotmail.com>
2018-06-22 22:09:22 +0100

    Improved error messaging and brought register page up to standard of login

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { User } from '../user.model';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,31 +12,58 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
-  email = new FormControl('', [Validators.required, Validators.email]);
   hide = true;
-  password = '';
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      username: ['', Validators.pattern(/^[a-z0-9]+$/i)],
+      username: ['', [Validators.required, Validators.pattern(/^[a-z0-9]+$/i)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(12)]]
     });
   }
 
   getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
+    return this.registerForm.controls.email.hasError('required') ? 'You must enter a value' :
+        this.registerForm.controls.email.hasError('email') ? 'Not a valid email' :
             '';
   }
 
   onSubmit() {
+    const user = new User(
+      this.registerForm.value.email,
+      this.registerForm.value.password,
+      this.registerForm.value.username,
+      null,
+      null
+    );
+    this.registerForm.disable();
+    this.userService.register(user).subscribe(
+      data => {
+        this.router.navigate(['/auth/login']);
+      },
+      error => {
+        this.registerForm.enable();
+        this.registerForm.setErrors({error: error});
+        this.registerForm.markAsPristine();
+      });
+  }
 
+  onTest() {
+    this.registerForm.setValue({
+      email: 'frank@example.com',
+      password: 'plainpassword',
+      username: 'Frank'
+    });
+    this.registerForm.markAsDirty();   // not pristine (as if value entered)
+  }
+
+  onClear() {
+    this.registerForm.reset();
   }
 
   onCancel() {
-    this.registerForm.reset();
+    this.router.navigate(['/']);
   }
 }
